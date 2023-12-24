4c559b327079a150228a74b241edd68aa0cff159
Kwangdo Yi <chungae9ri@gmail.com>
2023-12-24 13:50:20 -0500

    moved cmake toolchain file to cmake directory

---
+++ cmake/arm-none-eabi.cmake
@@ -0,0 +1,7 @@
+set(CMAKE_SYSTEM_PROCESSOR arm)
+set(CMAKE_C_COMPILER "arm-none-eabi-gcc")
+set(CMAKE_ASM_COMPILER "arm-none-eabi-gcc")
+set(CMAKE_LINKER "arm-none-eabi-gcc")
+set(CMAKE_CROSSCOMPILING true)
+set(CMAKE_SYSTEM_NAME Generic)
+
