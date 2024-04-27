4b39a4c981019ed9912755a86f1ad9d6124b5bfc
Kwangdo Yi <chungae9ri@gmail.com>
2024-04-26 21:28:47 -0400

    changed addr typedef name

--- kernel/inc/regops.h
+++ kernel/inc/regops.h
@@ -5,47 +5,47 @@
 #include <stdint.h>
 
 #if defined(__aarch64__)
-typedef uint64_t    arch_addr;
+typedef uint64_t    arch_addr_t;
 #else
-typedef uint32_t    arch_addr;
+typedef uint32_t    arch_addr_t;
 #endif
 
-static inline uint8_t read8(arch_addr addr)
+static inline uint8_t read8(arch_addr_t addr)
 {
     return (uint8_t)(*(volatile uint8_t *)addr);
 }
 
-static inline void write8(arch_addr addr, uint8_t val)
+static inline void write8(arch_addr_t addr, uint8_t val)
 {
     *(volatile uint8_t *)addr = val;
 }
 
-static inline uint16_t read16(arch_addr addr)
+static inline uint16_t read16(arch_addr_t addr)
 {
     return (uint16_t)(*(volatile uint16_t *)addr);
 }
 
-static inline void write16(arch_addr addr, uint16_t val)
+static inline void write16(arch_addr_t addr, uint16_t val)
 {
     *(volatile uint16_t *)addr = val;
 }
 
-static inline uint32_t read32(arch_addr addr)
+static inline uint32_t read32(arch_addr_t addr)
 {
     return (uint32_t)(*(volatile uint32_t *)addr);
 }
 
-static inline void write32(arch_addr addr, uint32_t val)
+static inline void write32(arch_addr_t addr, uint32_t val)
 {
     *(volatile uint32_t *)addr = val;
 }
 
-static inline uint64_t read64(arch_addr addr)
+static inline uint64_t read64(arch_addr_t addr)
 {
     return (uint64_t)(*(volatile uint64_t *)addr);
 }
 
-static inline void write64(arch_addr addr, uint64_t val)
+static inline void write64(arch_addr_t addr, uint64_t val)
 {
     *(volatile uint64_t *)addr = val;
 }
